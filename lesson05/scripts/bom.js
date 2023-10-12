f6a432c968d393051cdae82fac0ce8ddade81066
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2023-10-12 12:13:49 -0600

    Completed BOM Fav Chapter activity

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,26 @@
+const chapterInput = document.querySelector("#favchap");
+const addButton = document.querySelector("button");
+const chapterList = document.querySelector("#list");
+
+addButton.addEventListener("click", () => {
+    if (chapterInput === ""){
+        console.log("Error: Input is Blank");
+        chapterInput.focus();
+    }
+
+    else{
+        const newItem = chapterInput.value;
+        chapterInput.value = "";
+        const listItem = document.createElement("li")
+        const deleteItem = document.createElement("button");
+        
+        listItem.textContent = newItem;
+        chapterList.appendChild(listItem)
+        deleteItem.textContent = "❌";
+        listItem.appendChild(deleteItem);
+        deleteItem.addEventListener("click", () => {
+            chapterList.removeChild(listItem);
+        })
+        chapterInput.focus();
+    }
+})
