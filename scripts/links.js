8ef60f8abf1a4f6e357cf898d18d6e1ee78af17f
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2023-11-09 01:18:36 -0700

    Finished JSON Activity

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,31 @@
 const baseURL = "https://kjirstenhernandez.github.io/wdd230/";
 const linksURL = "https://kjirstenhernandez.github.io/wdd230/data/links.json";
+const mainList = document.querySelector(".list");
 
 async function getLinks(){
     const response = await fetch(linksURL);
     const data = await response.json();
     console.table(data);
+    displayLinks(data.lessons);
+}
+
+const displayLinks = (weeks) => {
+    weeks.forEach((weekLesson => {
+        const listItem = document.createElement("li");
+        listItem.innerHTML = `Learning Activity ${weekLesson.lesson}: `
+        let linkList = weekLesson.links;
+        linkList.forEach((link => {
+            const anchor = document.createElement("a");
+            anchor.setAttribute("href", link.url);
+            anchor.setAttribute("target", "_blank");
+            anchor.innerHTML = `${link.title} | `;
+            listItem.appendChild(anchor);
+            console.log(anchor)
+        }));
+        mainList.appendChild(listItem);
+        
+
+    }))
 }
 
 getLinks(baseURL);
