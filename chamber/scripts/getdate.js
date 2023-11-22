a28d86970dd7314423197427710b713eef3cf670
kjirstenhernandez <122486287+kjirstenhernandez@users.noreply.github.com>
2023-11-22 13:33:45 -0700

    Added wayfinding element

--- chamber/scripts/getdate.js
+++ chamber/scripts/getdate.js
@@ -1,12 +1,8 @@
 let oLastModif = new Date(document.lastModified);
 const modified = document.querySelector("#modified")
-console.log(oLastModif);
-
-
 
 // const options = {year:'numeric'};
 // const todaysDate = new Date();
 
 let lastModif = new Date(document.lastModified);
-console.log(document.lastModified);
 modified.innerHTML = `Last Modified: ${lastModif};`
